53b3f4e4ea9f075c001860c42ba4277b29811841
Kayk Edmar <kayksantana133@gmail.com>
2023-12-25 17:19:59 -0300

    att

--- github-favorites/js/Favorites.js
+++ github-favorites/js/Favorites.js
@@ -1,24 +1,79 @@
-// Classe que representa uma lista de favoritos
-export class Favorites { 
-
-  // Construtor recebe um seletor CSS chamado 'root'
-  constructor(root) { 
-    // Atribui ao 'root' o elemento no DOM correspondente ao seletor passado
+export class Favorites {
+  constructor(root) {
     this.root = document.querySelector(root);
+    this.load();
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: "maykbrito",
+        name: "Mayk Brito",
+        public_repos: "76",
+        followers: "12000",
+      },
+      {
+        login: "diego3g",
+        name: "Diego Fernandes",
+        public_repos: "76",
+        followers: "12000",
+      },
+    ];
   }
 }
 
-// Classe que estende a funcionalidade de Favorites
-// Trata da visualizaentos reção e evlacionados aos favoritos
-// A palavra-chave 'extends' é usada para herdar a funcionalidade da próxima classe
-export class FavoritesView extends Favorites { 
-
-  // Construtor recebe um seletor CSS chamado 'root'
-  constructor(root) { 
-    // Chama o construtor da classe pai (Favorites) com o 'root' fornecido
+export class FavoritesView extends Favorites {
+  constructor(root) {
     super(root);
-    
-    // Exibe no console o elemento associado ao 'root' da classe FavoritesView
-    console.log(this.root);
+
+    this.tbody = this.root.querySelector("table tbody");
+
+    this.update();
+  }
+
+  update() {
+    this.removeAllTr();
+
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user p").textContent = user.name;
+      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".repositories").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      this.tbody.append(row);
+    });
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td class="user">
+      <img src="https:github.com/maykbrito.png" alt="" />
+      <a href="https://github.com/maykbrito" target="_blank">
+        <p>Mayk Brito</p>
+        <span>maykbrito</span>
+      </a>
+    </td>
+    <td class="repositories">76</td>
+    <td class="followers">9589</td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+    `;
+
+    return tr;
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
   }
 }
